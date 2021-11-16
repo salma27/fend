f7c7ec2ba95fb98584177f69c6ee562860d2f3d4
salma27 <36279740+salma27@users.noreply.github.com>
2021-11-16 04:33:39 +0200

    section 4 is added

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,62 +1,63 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
+let sections = document.querySelectorAll("section");
+let sectionContent = sections[1].innerHTML;
+let section4 = document.createElement("section");
+section4.id = "section4";
+section4.setAttribute("data-nav", "Section 4");
+section4.innerHTML = sectionContent;
+section4.firstElementChild.firstElementChild.textContent = "Section 4";
 
+sections[2].insertAdjacentHTML("afterend", section4.outerHTML);
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
