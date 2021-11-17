e0ca3194dabb15a4365f53968e0185dcfd94ce20
salma27 <36279740+salma27@users.noreply.github.com>
2021-11-17 21:16:12 +0200

    rearrange + scrollTo

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -23,39 +23,47 @@
  *
  */
 
+let sections = document.querySelectorAll("section");
+let sectionContent = sections[0].innerHTML;
+let section4 = document.createElement("section");
+
+let frag = document.createDocumentFragment();
+let navBar = document.querySelector("nav");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
 /*
 adding the fourth section by copying the content of a previously made section,
 then altering the differences such as the id and the header content,
 and finally adding the new element as the last section.
 */
+function addSection() {
+    section4.id = "section4";
+    section4.setAttribute("data-nav", "Section 4");
+    section4.innerHTML = sectionContent;
+    section4.firstElementChild.firstElementChild.textContent = "Section 4";
+    sections[sections.length - 1].insertAdjacentHTML(
+        "afterend",
+        section4.outerHTML
+    );
+}
+addSection();
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
 
-let sections = document.querySelectorAll("section");
-let sectionContent = sections[0].innerHTML;
-let section4 = document.createElement("section");
-section4.id = "section4";
-section4.setAttribute("data-nav", "Section 4");
-section4.innerHTML = sectionContent;
-section4.firstElementChild.firstElementChild.textContent = "Section 4";
-sections[sections.length - 1].insertAdjacentHTML(
-    "afterend",
-    section4.outerHTML
-);
+// build the nav
 
 /**
  * creating navbar using a for loop that loops on all sections
- * and when clicked scrolls to the section
  */
-function scrolling(e) {
-    e.preventDefault();
 
-    let item = this.firstElementChild.getAttribute("href");
-    let top = document.querySelector(item).offsetTop;
-
-    scroll({
-        top: top,
-        behavior: "smooth",
-    });
-}
 function navData(section) {
     let newElement = document.createElement("li");
     newElement.innerHTML = `<a href="#${section.id}">${section.getAttribute(
@@ -65,31 +73,28 @@ function navData(section) {
     newElement.addEventListener("click", scrolling);
     frag.appendChild(newElement);
 }
-
-let frag = document.createDocumentFragment();
-let navBar = document.querySelector("nav");
 navBar.style.float = "right";
 sections = document.querySelectorAll("section");
 sections.forEach(navData);
 navBar.appendChild(frag);
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
+
+/**
+ * smoothly scroll when the item is clicked on navbar
+ */
+function scrolling(e) {
+    e.preventDefault();
+    let id = this.firstElementChild.getAttribute("href");
+    let section = document.querySelector(id);
+
+    scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+    });
+}
 
 /**
  * End Main Functions
